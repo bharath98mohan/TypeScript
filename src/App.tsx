8f0b10acede3c1980bc548c7c558ac9b1beac959
Bharath Mohan <bharath98mohan@gmail.com>
2022-09-12 16:16:09 +0530

    completed ep.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import './App.css';
-import { Greet } from './components/Greet';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+    {
+      first: "Princess",
+      last: "Diana",
+    },
+  ];
+
   return (
     <div className="App">
-      <Greet name='Bharath' />
+      <Greet name="Bharath" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
+      <Status status="success" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name='Bharath' isLoggedIn={false} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,18 @@
 type GreetProps = {
-    name: string
-}
+  name: string;
+  messageCount?: number;
+  isLoggedIn: boolean;
+};
 
 export const Greet = (props: GreetProps) => {
-    return (
-        <div>
-            <h2>Welcome {props.name} to TS</h2>
-        </div>
-    )
-}
+  const {messageCount = 0} = props
+  return (
+    <div>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${messageCount} unread messages`
+          : "Welcome Guest"}
+      </h2>
+    </div>
+  );
+};
