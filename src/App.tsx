83b59e625518d5a398b31c1b0ce92f2304fab40f
Bharath Mohan <bharath98mohan@gmail.com>
2022-09-13 14:33:21 +0530

    ep.7 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Greet } from "./components/Greet";
 import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
 import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
@@ -37,7 +40,14 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
       </Oscar>
-      <Greet name='Bharath' isLoggedIn={false} />
+      <Greet name="Bharath" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
